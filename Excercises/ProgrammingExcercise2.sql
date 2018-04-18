a5b504ae956facdbc4e17d316b4232a9f13d4c46
Ricardo Rosa <ricardo.y.rosa@outlook.com>
2018-04-18 15:46:09 -0400

    Updated

---
+++ Excercises/ProgrammingExcercise2.sql
@@ -0,0 +1,16 @@
+
+.echo on
+.headers on
+
+--Name: myfamily.sql
+
+--Author: Ricardo Rosa
+
+--Date: February 27, 2018
+
+
+Select title, firstname, lastname 
+Northwind Inc
+address
+city region postalcode country 
+from hr.Employees
